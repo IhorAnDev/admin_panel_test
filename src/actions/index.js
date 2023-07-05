8218f5b93f503cc25c8d8896945d149ac0b57e6a
IhorAnDev <ihorandev@gmail.com>
2023-07-05 17:17:10 +0300

    admin panel

---
+++ src/actions/index.js
@@ -0,0 +1,55 @@
+export const heroesFetching = () => {
+    return {
+        type: 'HEROES_FETCHING'
+    }
+}
+export const filtersFetching = () => {
+    return {
+        type: 'FILTERS_FETCHING'
+    }
+}
+export const heroesFetched = (heroes) => {
+    return {
+        type: 'HEROES_FETCHED',
+        payload: heroes
+    }
+}
+
+export const filtersFetched = (filters) => {
+    return {
+        type: 'FILTERS_FETCHED',
+        payload: filters
+    }
+}
+export const heroesFetchingError = () => {
+    return {
+        type: 'HEROES_FETCHING_ERROR'
+    }
+}
+
+export const filtersFetchingError = () => {
+    return {
+        type: 'FILTERS_FETCHING_ERROR'
+    }
+}
+
+export const activeFilterChanged = (filter) => {
+    return {
+        type: 'ACTIVE_FILTER_CHANGED',
+        payload: filter
+    }
+}
+export const heroDeleteFetch = (id) => {
+    return {
+        type: 'HERO_DELETE_FETCH',
+        payload: id
+    }
+}
+
+export const createHero = (hero) => {
+    return {
+        type: 'CREATE_HERO',
+        payload: hero
+    }
+}
+
